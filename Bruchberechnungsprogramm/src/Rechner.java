35ebaa820d5e80f095e0d8b49c2d0d3be38a44a4
Patric Auer <patric.auer@stud.hs-mannheim.de>
2013-10-15 15:54:13 +0200

    Wiederherstellung

---
+++ Bruchberechnungsprogramm/src/Rechner.java
@@ -0,0 +1,62 @@
+public class Rechner {
+
+	public static void main(String[] args) {
+
+		Bruch b1 = new Bruch(9, 24);
+		Bruch b2 = new Bruch(4, 3);
+		Bruch b3 = new Bruch(1, 1, 3);
+		Bruch b4 = new Bruch(8);
+
+		// System.out.println(b2);
+
+		Bruch addi = b2.addiere(b3);
+		System.out.println(addi);
+
+		// Bruch subbi = b1.subtrahiere(b2);
+		// System.out.println(subbi);
+
+		// Bruch multi = b1.multipliziere(b2);
+		// System.out.println(multi);
+
+		// Bruch kehr = b1.kehrwert();
+		// System.out.println(kehr);
+
+		// Bruch div = b1.dividieren(b2);
+		// System.out.println(div);
+
+		// Bruch pot = b1.potenzieren(5);
+		// System.out.println(pot);
+
+		// double dezi = b1.getDezimalzahl();
+		// System.out.println(dezi);
+
+		// boolean echt = b1.isEcht();
+		// System.out.println(echt);
+
+		// boolean equ = b1.equals(b2);
+		// System.out.println(equ);
+
+		// Bruch kur = b1.kuerzen(b2);
+		// System.out.println(kur);
+
+		// long za = b1.getZaehler();
+		// System.out.println(za);
+
+		// long nen = b1.getNenner();
+		// System.out.println(nen);
+
+		// Bruch ech = b1.echterBruch();
+		// System.out.println(ech);
+
+		// Bruch unb = b3.unechterBruch();
+		// System.out.println(unb);
+
+		// Bruch mult = b2.multipliziere(b4);
+		// System.out.println(mult);
+		//
+		// Bruch divi = b2.dividieren(b4);
+		// System.out.println(divi);
+
+	}
+
+}
